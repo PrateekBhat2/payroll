8db1dda179ea74666c0513a97fe4906d6abf0bb4
Prateek Bhat <prateek.bhat@ukg.com>
2024-08-13 12:16:55 +0530

    add validation

--- src/main/java/com/ukg/payroll/entity/Payroll.java
+++ src/main/java/com/ukg/payroll/entity/Payroll.java
@@ -3,6 +3,8 @@ package com.ukg.payroll.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,14 @@ import lombok.Setter;
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Payroll extends BaseEntity{
     @Id
+    @NotNull(message = "transactionId cannot be null")
     private Long transactionId;
+
+    @NotNull(message = "employeeId cannot be null")
     private Long employeeId;
+
     private String payrollDate;
+
+    @PositiveOrZero
     private Long finalAmount;
 }
