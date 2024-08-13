39f95980621c7b4157197e46d3a2f306cca3ed5e
Prateek Bhat <prateek.bhat@ukg.com>
2024-08-13 12:31:20 +0530

    add documentation

--- src/main/java/com/ukg/payroll/PayrollApplication.java
+++ src/main/java/com/ukg/payroll/PayrollApplication.java
@@ -1,11 +1,26 @@
 package com.ukg.payroll;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Payroll REST API",
+				description = "REST CRUD Operation for Payroll",
+				contact = @Contact(
+						name = "John Doe",
+						email = "john@synergybank.com",
+						url = "http://www.synergybank.com"
+				)
+
+		)
+)
 public class PayrollApplication {
 
 	public static void main(String[] args) {
